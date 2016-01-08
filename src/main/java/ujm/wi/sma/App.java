975b5ecb715b88f049d39f0484a9aacc9b38d582
Bachir31 <bouacheria.bachir@hotmail.fr>
2016-01-08 09:07:59 +0100

    [BBA] first commit

---
+++ src/main/java/ujm/wi/sma/App.java
@@ -0,0 +1,13 @@
+package ujm.wi.sma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
