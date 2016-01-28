6947c4335ddd3430b96b6ad9031daf12c47d6123
Yassine <yassine@to-z.com>
2016-01-28 12:16:43 +0100

    paths linux

--- src/main/java/ujm/wi/enums/Paths.java
+++ src/main/java/ujm/wi/enums/Paths.java
@@ -1,10 +1,14 @@
 package ujm.wi.enums;
 
 public enum Paths {
-	VAR("C:\\FullRLFAP\\CELAR\\scen02\\var.txt"), DOM("C:\\FullRLFAP\\CELAR\\scen02\\dom.txt"), 
-	CTR("C:\\FullRLFAP\\CELAR\\scen02\\ctr.txt"), JCM("C:\\FullRLFAP\\outFiles\\file.jcm"),
-	ASL("C:\\FullRLFAP\\outFiles\\file.asl");
+//	VAR("C:\\FullRLFAP\\CELAR\\scen02\\var.txt"), DOM("C:\\FullRLFAP\\CELAR\\scen02\\dom.txt"), 
+//	CTR("C:\\FullRLFAP\\CELAR\\scen02\\ctr.txt"), JCM("C:\\FullRLFAP\\outFiles\\file.jcm"),
+//	ASL("C:\\FullRLFAP\\outFiles\\file.asl");
 
+	VAR("/home/yassine/FullRLFAP/CELAR/scen02/var.txt"), DOM("/home/yassine/FullRLFAP/CELAR/scen02/dom.txt"), 
+	CTR("/home/yassine/FullRLFAP/CELAR/scen02/ctr.txt"), JCM("/home/yassine/FullRLFAP/outFiles/file.jcm"),
+	ASL("/home/yassine/FullRLFAP/outFiles/file.asl");
+	
 	private String name = "";
 
 	Paths(String name) {
